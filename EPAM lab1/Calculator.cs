18e315b7e34a072cd7b300ddf10579596d4f19a6
Nastya Oleferovich <Nastya23.05@mail.ru>
2016-09-13 14:46:07 +0300

    Oleferovich

---
+++ EPAM lab1/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_lab1
+{
+    class Calculator
+    {
+      
+        double result = 0;
+        private double Sum(double x1, double x2) // сложение
+        {  
+           return x1 + x2;                     
+        }
+
+        private double Sub(double x1, double x2) // вычитание
+        {
+            return  x1 - x2;
+        }
+
+        private double Mult(double x1, double x2) // умножение
+        {
+            return  x1 * x2;
+        }
+
+        private double Div(double x1, double x2) // деление
+        {
+            if (x2 == 0) { Console.WriteLine("Второе значение равно нулю"); }
+            return  x1 / x2;
+        }
+        public double ResultFuction(double x1, double x2, int z)  // Результирующая функция
+        {      
+            switch (z)
+            {
+                case 1: result = Sum(x1, x2); break;
+                case 2: result = Sub(x1, x2); break;
+                case 3: result = Mult(x1, x2); break;
+                case 4: result = Div(x1, x2); break;
+            }
+            return result;
+
+        }
+    }
+
+    }
+
---
+++ EPAM lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_lab1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Calculator function = new Calculator();
+            double x1, x2;
+            int numfun, maxnumfun = 4, minnumfun = 0;
+            
+            try
+            {
+                Console.WriteLine("Выберите операцию (1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление)");
+
+                numfun = Convert.ToInt32(Console.ReadLine());
+                if (numfun > maxnumfun || numfun < minnumfun)
+                {
+                    Console.WriteLine("Данной операции не существует");
+                    Console.WriteLine("Выберите операцию (1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление)");
+                    numfun = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("Первое число:");
+                x1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Второе число");
+                x2 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Результат = " + function.ResultFuction(x1, x2, numfun));
+            }
+            catch (Exception e)
+            { Console.WriteLine("Неверный тип введёных значений." + e.Message); }
+           
+       
+           
+        }
+
+    }
+}
